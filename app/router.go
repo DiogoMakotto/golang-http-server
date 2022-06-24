8016b8f9f41df1df8517f77839c9a7ff0b57e8b0
DiogoMakotto <diogomakotto@gmail.com>
2022-06-24 17:54:42 -0300

    refactor

--- app/router.go
+++ app/router.go
@@ -3,14 +3,10 @@ package app		//nome do pacote
 import ( 		//imports necessários
 	"net/http"
 	"log"
-	"fmt"
+	"golang-http-server/app/modules"
 )
 
 func Routes(){		//função para declarar endpoints
-	http.HandleFunc("/", returnString)
+	http.HandleFunc("/", modules.ReturnString)
 	log.Fatal(http.ListenAndServe(":8089", nil))
-}
-
-func returnString(w http.ResponseWriter, r *http.Request){		//função que recebe os retornos de parametos
-	fmt.Fprintf(w, "testing some data as response =P")
 }
